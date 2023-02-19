81f248d137365d5471c63a68b6f09816dee061f1
FranciscaMasombo <frmm97@gmail.com>
2023-02-19 12:05:12 +0000

    All methods needed

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+    private ArrayList<Person> address;
+
+    public void addAddress(){
+
+    }
+    public void updateAddress(){
+
+    }
+    public void deleteAddress(){
+
+    }
+    public void sortByFname(){
+
+    }
+    public void sortByLname(){
+
+    }
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,17 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String fulladresss;
+
+    public Person(String firstName, String lastName, String phoneNumber, String email, String fulladresss) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.fulladresss = fulladresss;
+    }
+
+
 }
